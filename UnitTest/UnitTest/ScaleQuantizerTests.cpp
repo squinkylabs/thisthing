ff786f4e7571e323edc7032604f628bcf6e86d1b
squinkylabs <squinkylabs@users.noreply.github.com>
2016-03-02 18:03:54 -0800

    logic for in between quantized notes

--- UnitTest/UnitTest/ScaleQuantizerTests.cpp
+++ UnitTest/UnitTest/ScaleQuantizerTests.cpp
@@ -38,14 +38,37 @@ static void sq0()
 void sq1()
 {
 	{
+		// out note is "past the end"
 		char scale[] = {3, -1};
 		int q = ScaleQuantizer::quantize_semi(11, scale);
 		assert(q == 3);
 	}
+	{
+		// out note is "before the start"
+		char scale[] = {11, -1};
+		int q = ScaleQuantizer::quantize_semi(3, scale);
+		assert(q == 11);
+	}
 }
+
+// basic tests for "in between" notes
+void sq2()
+{
+	printf("sq2, a\n");
+	char scale[] = {3, 7, 11, -1};
+	int q = ScaleQuantizer::quantize_semi(4, scale);
+	assert(q == 3);		// 4 is close to 3
+
+	printf("sq2, b\n");
+	q = ScaleQuantizer::quantize_semi(6, scale);
+	assert(q == 7);		// 6 is close to 7
+
+}
+
 
 void ScaleQuantizerTests()
 {
 	sq0();
 	sq1();
+	sq2();
 }
--- common/ScaleQuantizer.h
+++ common/ScaleQuantizer.h
@@ -25,10 +25,17 @@ public:
 		}
 #endif
 		char ret = 0;
+		char last_scale_entry = -1;
 		for (bool done=false; !done; ++scale)
 		{
 			if (*scale < 0)
 			{
+				// if scale over and we found nothing, pick last entry.
+				// will not handle wrap around, but OK for now
+				if (last_scale_entry >= 0)
+				{
+					ret = last_scale_entry;
+				}
 				printf("terminate on end of scale, ret = %d\n", ret);
 				done = true;
 			}
@@ -38,6 +45,28 @@ public:
 				done = true;
 				printf("terminate on match, ret = %d\n", ret);
 			}
+#if 1
+			else if (semi < *scale)
+			{
+				printf("scale search went past semi note\n");
+				if (last_scale_entry >= 0)
+				{
+					printf("but there was a previous one\n");
+
+					const int dprev = semi - last_scale_entry;
+					const int dnext = *scale - semi;
+					printf("dprev=%d dnext=%d last=%d, scale=%d semi=%d\n", dprev, dnext, last_scale_entry, *scale, semi); 
+					assert(dprev > 0 && dnext > 0);
+
+					ret = (dprev <= dnext) ? last_scale_entry : *scale;
+					done = true;
+					printf(" leave on in between %d\n", ret);
+
+
+				}
+			}
+#endif
+			last_scale_entry = *scale;
 		}
 		return ret;
 	}
