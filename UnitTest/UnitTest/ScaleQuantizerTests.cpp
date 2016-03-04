6a30de7513632411b45a703dbd5ce85477fc8dc7
squinkylabs <squinkylabs@users.noreply.github.com>
2016-03-03 18:10:57 -0800

    scale quantize func working

--- UnitTest/UnitTest/ScaleQuantizerTests.cpp
+++ UnitTest/UnitTest/ScaleQuantizerTests.cpp
@@ -3,51 +3,117 @@
 
 #include "ScaleQuantizer.h"
 
+
+
+static char quantize_semi(int semi, const char * scale)
+{
+	// first expand the scale
+
+
+	char expanded[13];
+	char first = *scale;
+	char last = 0;
+
+	printf("init scale: ");
+	for (const char *p=scale; *p != -1; ++p)
+	{
+		last = *p;
+		printf("%d,", last);
+	}
+	printf("\n");
+
+	char * outp = expanded;
+
+	int len = 0;
+	// expand first entry, if needed
+	if (first > 0)
+	{
+		printf("first > 0\n");
+		*outp++ = last - 12;
+		++len;
+	}
+	
+	for (const char *p=scale; *p != -1; ++p)
+	{
+		*outp++ = *p;
+		++len;
+	}
+
+	if (last < 11)
+	{
+		*outp++ = first + 12;
+		++len;
+	}
+
+	
+
+	printf("expanded: ");
+	
+	for (int i=0; i<len; ++i)
+	{
+		printf("%d,", expanded[i]);
+
+	}
+	printf("\n len=%d\n", len);
+
+
+
+
+	// now quantize it
+	return  ScaleQuantizer::quantize_semi_expanded(semi, expanded, len);
+}
+
 // test some simple case: ScaleQuantizer::quantize_semi
 // single note in scale
 static void sq0()
 {
+	printf("sq0\n");
 	{
 	char scale[] = {0, -1};
-	int q = ScaleQuantizer::quantize_semi(0, scale);
+	int q = quantize_semi(0, scale);
 	assert(q == 0);
 	}
 
+	printf("sq0-b\n");
 	{
 	char scale2[] = {6, -1};
-	int q = ScaleQuantizer::quantize_semi(6, scale2);
+	int q = quantize_semi(6, scale2);
 	assert(q == 6);
 	}
 
+	printf("sq0-c\n");
 	{
 	char scale3[] = {6, 7, 8, 11, -1};
-	int q = ScaleQuantizer::quantize_semi(7, scale3);
+	int q = quantize_semi(7, scale3);
 	assert(q == 7);
-	q = ScaleQuantizer::quantize_semi(6, scale3);
+	q = quantize_semi(6, scale3);
 	assert(q == 6);
-	q = ScaleQuantizer::quantize_semi(8, scale3);
+	q = quantize_semi(8, scale3);
 	assert(q == 8);
-	q = ScaleQuantizer::quantize_semi(11, scale3);
+	q = quantize_semi(11, scale3);
 	assert(q == 11);
 	}
 }
 
 
 // test  ScaleQuantizer::quantize_semi
-// single note note in scale
+// single note note in scale, wrap octaves
 void sq1()
 {
+	printf("sq1\n");
 	{
 		// out note is "past the end"
 		char scale[] = {3, -1};
-		int q = ScaleQuantizer::quantize_semi(11, scale);
-		assert(q == 3);
-	}
+		int q = quantize_semi(11, scale);
+		assert(q == 3+12); // wrap next octave
+		}
+	printf("sq1-b\n");
 	{
 		// out note is "before the start"
 		char scale[] = {11, -1};
-		int q = ScaleQuantizer::quantize_semi(3, scale);
-		assert(q == 11);
+		int q = quantize_semi(3, scale);
+		printf("q =%d\n", q);
+		assert(q == 11-12);		// wraps under
 	}
 }
 
@@ -56,13 +122,22 @@ void sq2()
 {
 	printf("sq2, a\n");
 	char scale[] = {3, 7, 11, -1};
-	int q = ScaleQuantizer::quantize_semi(4, scale);
+	int q = quantize_semi(4, scale);
 	assert(q == 3);		// 4 is close to 3
 
 	printf("sq2, b\n");
-	q = ScaleQuantizer::quantize_semi(6, scale);
+	q = quantize_semi(6, scale);
 	assert(q == 7);		// 6 is close to 7
 
+}
+
+// basic test for octave wrap around
+void sq3()
+{
+	printf("sq3\n");
+	char scale[] = { 1, 2, -1};
+	int q = quantize_semi(11, scale);
+	assert(q == 1+12);		// 1 is "pretty close" to 11 if you wrap octaves
 }
 
 
@@ -71,4 +146,5 @@ void ScaleQuantizerTests()
 	sq0();
 	sq1();
 	sq2();
+	sq3();
 }
--- common/ScaleQuantizer.h
+++ common/ScaleQuantizer.h
@@ -6,69 +6,95 @@ class ScaleQuantizer
 public:
 	/*  semi is 0..11 normalized semitone.
 	 * scale is a list of normalized semitones, terminated with < 0 entry
+	 * scale is "expanded" such that the the lowest entry is <=0 and the highest is >=11
+	 *
 	 * return is nearest semi in scale, NOT normalized
 	 * if note not in scale, will pick nearest.
 	 * if equal distance, will round down
 	 */
-	static char quantize_semi(char semi, char * scale)
+	static char quantize_semi_expanded(char semi, const char * _scale, int scale_length)
 	{
-#ifdef _DEBUG
-		assert(semi >= 0 && semi <= 11);
-		int last = -1;
-		for (char * p = scale; *p >= 0; ++p)
+		assert(check_expanded_scale(_scale, scale_length));
+
+		for (int scale_index = 0; true; ++scale_index)
 		{
-			int x = *p;
-			assert (x >= 0 && x <= 11);
-			assert (x > last);
+			
+			// if we went off the end of the scale, something is wrong
+			if (scale_index >= scale_length)
+			{
+				assert(false);
+				return 0;			
+
+			}
+
+			const char scale_semi = _scale[scale_index];
+
+			// if we are on the target, we are golden
+			if (scale_semi == semi)
+			{
+	
+				printf("terminate on match, ret = %d\n", semi);
+				return semi;
+			}
+
+			// if we need to look further, there better be a further
+			if (scale_index >= (scale_length-1))
+			{
+				assert(false);
+				return 0;
+			}
+
+			const char next_scale_semi = _scale[scale_index+1];
+
+			// if our semi is now in betwee, current and next, we can pick one
+			if (semi < next_scale_semi)
+			{
+
+				const int dprev = semi - scale_semi;
+				const int dnext = next_scale_semi - semi;
+				//printf("dprev=%d dnext=%d last=%d, scale=%d semi=%d\n", dprev, dnext, scale_semi, , semi); 
+				assert(dprev > 0 && dnext > 0);
+
+				return (dprev <= dnext) ? scale_semi : next_scale_semi;
+				//done = true;
+				//printf(" leave on in between %d\n", ret);
+			}
+
+			//last_scale_entry = *scale;
+		}
+		assert(false);
+		return 0;
+	}
+
+	static bool check_expanded_scale(const char * scale, int length)
+	{
+		// make sure it's increasing
+		int last = -100000000;
+
+		for (int i=0; i<length; ++i)
+		{
+			int x = scale[i];
+			if (x <= last)
+			{
+				printf("scale not increasing x=%d last=%d\n", x, last);
+				return false;
+			}
+			
 			last = x;
-
 		}
-#endif
-		char ret = 0;
-		char last_scale_entry = -1;
-		for (bool done=false; !done; ++scale)
+
+		if (scale[0] > 0)
 		{
-			if (*scale < 0)
-			{
-				// if scale over and we found nothing, pick last entry.
-				// will not handle wrap around, but OK for now
-				if (last_scale_entry >= 0)
-				{
-					ret = last_scale_entry;
-				}
-				printf("terminate on end of scale, ret = %d\n", ret);
-				done = true;
-			}
-			else if (*scale == semi)
-			{
-				ret = semi;
-				done = true;
-				printf("terminate on match, ret = %d\n", ret);
-			}
-#if 1
-			else if (semi < *scale)
-			{
-				printf("scale search went past semi note\n");
-				if (last_scale_entry >= 0)
-				{
-					printf("but there was a previous one\n");
-
-					const int dprev = semi - last_scale_entry;
-					const int dnext = *scale - semi;
-					printf("dprev=%d dnext=%d last=%d, scale=%d semi=%d\n", dprev, dnext, last_scale_entry, *scale, semi); 
-					assert(dprev > 0 && dnext > 0);
-
-					ret = (dprev <= dnext) ? last_scale_entry : *scale;
-					done = true;
-					printf(" leave on in between %d\n", ret);
-
-
-				}
-			}
-#endif
-			last_scale_entry = *scale;
+			printf("not expanded on bottom\n");
+			return false;
 		}
-		return ret;
+		if (last < 11)
+		{
+			printf("not expanded on top, last = %d\n", last);
+			return false;
+		}
+		
+		return true;
 	}
 };
 #endif
