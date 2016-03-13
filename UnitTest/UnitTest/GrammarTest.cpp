1238ff88658bae5c21e2d9d42a4b819d567c2bba
squinkylabs <squinkylabs@users.noreply.github.com>
2016-03-13 10:30:37 -0700

    more

--- UnitTest/UnitTest/GrammarTest.cpp
+++ UnitTest/UnitTest/GrammarTest.cpp
@@ -5,47 +5,83 @@
 #include "Random.h"
 
 static const int numRules = 3;
-//static Random r;
+static Random r;
 
 
 
 static ProductionRule rules[numRules]; 
 
-static void init0()
+static GKEY init0()
 {
-	// always do zero
+	// This rule always generate sg-w2 (two whole notes tied together)
+	ProductionRule& r = rules[sg_w2];
 
-#if 1
-	ProductionRule& r = rules[0];
-	r.entries[0].probability = (unsigned char)255;
-	r.entries[0].code = (unsigned char) 0;
-#else
-	ProductionRule * r = rules;
-	r->entries[0].probability = (unsigned char)255;
-	r->entries[0].code = (unsigned char) 0;
-
-	printf("now %d\n", r->entries[0].probability);
-#endif
+	// why isn't this entries[0]?
+	r.entries[1].probability = (unsigned char)255;
+	r.entries[1].code = sg_invalid;		// always terminate
 
 	printf("just set prob to 255: %d\n", rules[0].entries[0].probability);
 
-
+	return sg_w2;
 }
 
 
 
 void gt0()
 {
-	init0();
-//	rules[0].evaluate();
+	GKEY init = init0();
+
 	Random r;
 	ProductionRule::EvaluationState es(r);
 	es.rules = rules;
 	es.numRules = numRules;
-	ProductionRule::evaluate(es, 0);
+	ProductionRule::evaluate(es, init);
+}
+
+
+
+static GKEY init1()
+{
+	printf("in init1 making 50/50 for %d\n", sg_w2);
+	{
+	// start with w2 durration
+	ProductionRule& r = rules[sg_w2];
+
+	// break into w,w prob 50
+
+	r.entries[0].probability = (unsigned char)255 /2;
+	r.entries[0].code = sg_ww;		
+	r.entries[1].probability = (unsigned char)255;
+	r.entries[1].code = sg_invalid;		// always terminate
+	}
+
+	{
+	// now need rule for w hole
+	printf("in init1 making 100 for %d\n", sg_w);
+	 ProductionRule& r = rules[sg_w];
+	 r.entries[1].probability = (unsigned char)255;
+	 r.entries[1].code = sg_invalid;		// always terminate
+	}
+	 printf("leave init 1. rule 1 p0 = %d\n",  rules[sg_w2].entries[0].probability);
+
+	return sg_w2;
+}
+
+
+void gt1()
+{
+	GKEY init = init1();
+
+	
+	ProductionRule::EvaluationState es(r);
+	es.rules = rules;
+	es.numRules = numRules;
+	ProductionRule::evaluate(es, init);
 }
 void GrammarTest()
 {
-	gt0();
+	//gt0();
+	for (int i=0; i<10; ++i)
+		gt1();
 }
 
--- common/StochasticGrammar.h
+++ common/StochasticGrammar.h
@@ -3,12 +3,84 @@
 
 #include "Random.h"
 
+/***********************************************
+ ********** rhytmic grouping codes *************
+************************************************/
+typedef unsigned char GKEY;
+
+// TODO: need to re-think these. Go back to haveing the keys be the table index, too?
+// in that case, don't need terminalValue any more....
+
+const GKEY sg_invalid = 0;		// either uninitialized rule, or return value that stops recursion.
+								// Note that this means table of production rules must have a dummy entry up front
+const GKEY sg_w2	= 1;		// duration of two whole notes
+const GKEY sg_w		= 2;		// whole
+const GKEY sg_ww	= 3;		// w,w
+const GKEY sg_h		= 4;		
+const GKEY sg_hh	= 5;
+const GKEY sg_q		= 6;
+const GKEY sg_e		= 7;
+const GKEY sg_ee	= 8;
+
+const int PPQ = 96;
+
+class ProductionRuleKeys
+{
+public:
+	// turn a key into a 0 terminated list of keys for individual notes
+	static void breakDown(GKEY key, GKEY * outKeys, int outSize);
+
+	// get the duration in clocks for a "simple" key (like q, not q+q)
+	static int getDuration(GKEY key);
+
+	static const char * toString(GKEY key);
+};
+
+inline const char * ProductionRuleKeys::toString(GKEY key)
+{
+	const char * ret;
+	switch(key)
+	{
+	case sg_w2:
+		ret = "2xw";
+		break;
+
+	default:
+		printf("can't print key %d\n", key);
+		assert(false);
+		ret="error";
+	}
+	return ret;
+
+}
+
+
+inline int ProductionRuleKeys::getDuration(GKEY key)
+{
+	int ret;
+	switch(key)
+	{
+	case sg_w2:
+		ret = 2 * 4 * PPQ;
+		break;
+
+	default:
+		assert(false);
+		ret=0;
+	}
+	return ret;
+
+}
+/**************************************************************************
+
+**************************************************************************/
+
 class ProductionRuleEntry
 {
 public:
-	ProductionRuleEntry() : probability(0), code(0) {}
+	ProductionRuleEntry() : probability(0), code(sg_invalid) {}
 	unsigned char probability;	// 1..256
-	unsigned char code;			// what to do if this one fires
+	GKEY code;			// what to do if this one fires
 };
 
 class ProductionRule
@@ -24,12 +96,19 @@ public:
 		Random& r;		//random number generator to use 
 	};
 
-	ProductionRule() {}
+	ProductionRule()  {}
+
+	/* the data
+	 */
+
+	// each possible production rule for this state
 	ProductionRuleEntry entries[numEntries];
+//	GKEY terminalValue;		// if nothing produced, this is the output (entire time duration of interval)
+
 	static void evaluate(const EvaluationState& es, int ruleToEval);
 private:
 	static int _evaluateRule(const ProductionRule& rule, int random);
-	bool _isValid() const;
+	bool _isValid(int index) const;
 };
 
 
@@ -38,19 +117,22 @@ inline int ProductionRule::_evaluateRule(const ProductionRule& rule, int random)
 {
 	//int rand = r.get() & 0xff;
 	int rand = random & 0xff;
-	printf("rand is %d\n", rand);
+	printf("evaluateRule called with rand is %d\n", rand);
 		//for (int i=0; i<numEntries; ++i)
 
 	int i=0;
 	for (bool done2=false; !done2; ++i )
 	{
 		assert(i<numEntries);
-		printf("prob is %d\n",  rule.entries[i].probability);
+		printf("prob[%d] is %d\n", i,  rule.entries[i].probability);
 		if ( rule.entries[i].probability >= rand)
 		{
+			GKEY code = rule.entries[i].code;
+			printf("rule fired on code abs val=%d\n", code);
 			// rule fired!
-			printf("rule fired! execute code %d\n",  rule.entries[i].code);
-			return rule.entries[i].code;
+			//printf("rule fired! execute code %s\n",  ProductionRuleKeys::toString(code));
+			//printf("TODO: generate note dur %d\n",  ProductionRuleKeys::getDuration(code));
+			return code;
 		}
 	}
 	assert(false);	// no rule fired
@@ -59,23 +141,34 @@ inline int ProductionRule::_evaluateRule(const ProductionRule& rule, int random)
 
 inline void ProductionRule::evaluate(const EvaluationState& es, int ruleToEval)
 {
+	printf("\n evaluate called on rule #%d\n", ruleToEval);
 	const ProductionRule& rule = es.rules[ruleToEval];
-	assert(rule._isValid());
-	int result = _evaluateRule(rule, es.r.get());
-	if (result == 0)
+	assert(rule._isValid(ruleToEval));
+	GKEY result = _evaluateRule(rule, es.r.get());
+	if (result == sg_invalid)		// request to remindate recrsion
 	{
+		GKEY code = ruleToEval;		// our "real" terminal code is our table index
 		printf("production rule #%d terminated\n", ruleToEval);
+		printf("rule terminated! execute code %s\n",  ProductionRuleKeys::toString(code));
+		printf("TODO: generate note dur %d\n",  ProductionRuleKeys::getDuration(code));
 		printf("TODO: need to produce something\n");
 	}
 	else
 	{
 		printf("production rule #%d expanded to %d\n", ruleToEval, result);
+		// need to expand,then eval all of the expanded codes
 		assert(false);
 	}
 }
 			
-inline bool ProductionRule::_isValid() const 
+// is the data self consistent, and appropriate for index
+inline bool ProductionRule::_isValid(int index) const 
 {
+	if (index == sg_invalid)
+	{
+		printf("rule not allowed in first slot\n");
+		return false;
+	}
 	int last = -1;
 	for (int i=0; i<numEntries; ++i)
 	{
@@ -89,6 +182,7 @@ inline bool ProductionRule::_isValid() const
 			return true;					// must have a 255 to end it			
 	}
 	printf("not terminated\n");
+	// TODO: test that all rules add up to terminalValue
 	return false;
 }
 
